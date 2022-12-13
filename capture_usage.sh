dbbacc10a1ac80577d56a80bc27d201df8591dbf
Karan <karan.batavia@privado.ai>
2022-12-13 22:16:23 +0530

    changed time interval for usage capture

--- capture_usage.sh
+++ capture_usage.sh
@@ -3,6 +3,6 @@ do
 top -b -n1 > ${GITHUB_WORKSPACE}/tools/output.txt;
 cat ${GITHUB_WORKSPACE}/tools/output.txt | grep -a '%Cpu(s)' >> ${GITHUB_WORKSPACE}/tools/results_1.txt;
 cat ${GITHUB_WORKSPACE}/tools/output.txt | grep -a 'MiB Mem' >> ${GITHUB_WORKSPACE}/tools/results_1.txt;
-sleep 60;  # time after which to run the command
+sleep 10;  # time after which to run the command
 done
 
